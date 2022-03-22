0e420f3d6f0a4579c1ac49562a99047c2726a112
Orangexua <chenzixu@MacintoshdeMBP.lan>
2022-03-22 14:20:35 +0800

    login update

--- granduation_fe/src/App.vue
+++ granduation_fe/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div id="app">
+    <ZhiHuIndex/>
   </div>
 </template>
+
+<script>
+import ZhiHuIndex from './components/ZhiHuIndex'
+
+export default {
+  name: 'App',
+  components: {
+    ZhiHuIndex
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ granduation_fe/src/components/ZhiHuIndex.vue
@@ -0,0 +1,30 @@
+<template>
+  <component :toggleComponent="toggleComponent" :is="typeName"></component>
+</template>
+
+<script>
+export default {
+  name: 'ZhiHuIndex',
+  data () {
+    return {
+      typeName: 'Login'
+    }
+  },
+  methods:{
+    toggleComponent:function(){
+      this.typeName = this.typeName === "Login" ? "Register" : "Login"
+    }
+  },
+  components:{
+    Login,
+    Register
+  }
+}
+import Login from "../views/Login.vue"
+import Register from "../views/Register"
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
