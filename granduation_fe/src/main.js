a1750d75ca767c20ad48f70275f6f433ed7f9073
Orangexua <chenzixu@MacintoshdeMacBook-Pro.local>
2022-03-29 14:07:00 +0800

    index dependence

--- granduation_fe/src/main.js
+++ granduation_fe/src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App);
 app.config.globalProperties.axios = axios;
-app.use(router).use(store).mount('#app');
+app.use(router).use(store).use(ElementPlus).mount('#app');
